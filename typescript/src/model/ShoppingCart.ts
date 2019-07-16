ed286b0c430afca165c3c986cbfe4af35df00f08
Gerrie Swart <g.swart@vehikl.com>
2019-07-16 14:50:42 -0400

    Removed unused methods

--- typescript/src/model/ShoppingCart.ts
+++ typescript/src/model/ShoppingCart.ts
@@ -8,20 +8,16 @@ import {Offer} from "./Offer"
 import {SpecialOfferType} from "./SpecialOfferType"
 
 type ProductQuantities = { [productName: string]: ProductQuantity }
-export type OffersByProduct = {[productName: string]: Offer};
+export type OffersByProduct = { [productName: string]: Offer };
 
 export class ShoppingCart {
 
-    private readonly  items: ProductQuantity[] = [];
+    private readonly items: ProductQuantity[] = [];
     _productQuantities: ProductQuantities = {};
 
 
     getItems(): ProductQuantity[] {
         return _.clone(this.items);
-    }
-
-    addItem(product: Product): void {
-        this.addItemQuantity(product, 1.0);
     }
 
     productQuantities(): ProductQuantities {
@@ -30,31 +26,23 @@ export class ShoppingCart {
 
 
     public addItemQuantity(product: Product, quantity: number): void {
-        let productQuantity = new ProductQuantity(product, quantity)
+        let productQuantity = new ProductQuantity(product, quantity);
         this.items.push(productQuantity);
-        let currentQuantity = this._productQuantities[product.name]
-        if (currentQuantity) {
-            this._productQuantities[product.name] = this.increaseQuantity(product, currentQuantity, quantity);
-        } else {
-            this._productQuantities[product.name] = productQuantity;
-        }
+        let currentQuantity = this._productQuantities[product.name];
+        this._productQuantities[product.name] = productQuantity;
 
     }
 
-    private increaseQuantity(product: Product, productQuantity: ProductQuantity, quantity: number) {
-        return new ProductQuantity(product, productQuantity.quantity + quantity)
-    }
-
-    handleOffers(receipt: Receipt,  offers: OffersByProduct, catalog: SupermarketCatalog ):void {
+    handleOffers(receipt: Receipt, offers: OffersByProduct, catalog: SupermarketCatalog): void {
         for (const productName in this.productQuantities()) {
             const productQuantity = this._productQuantities[productName]
             const product = productQuantity.product;
             const quantity: number = this._productQuantities[productName].quantity;
             if (offers[productName]) {
-                const offer : Offer = offers[productName];
-                const unitPrice: number= catalog.getUnitPrice(product);
+                const offer: Offer = offers[productName];
+                const unitPrice: number = catalog.getUnitPrice(product);
                 let quantityAsInt = quantity;
-                let discount : Discount|null = null;
+                let discount: Discount | null = null;
                 let x = 1;
                 if (offer.offerType == SpecialOfferType.ThreeForTwo) {
                     x = 3;
@@ -67,7 +55,8 @@ export class ShoppingCart {
                         discount = new Discount(product, "2 for " + offer.argument, discountN);
                     }
 
-                } if (offer.offerType == SpecialOfferType.FiveForAmount) {
+                }
+                if (offer.offerType == SpecialOfferType.FiveForAmount) {
                     x = 5;
                 }
                 const numberOfXs = Math.floor(quantityAsInt / x);
