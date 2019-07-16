3fdddf84bd256f79e26865c07a3d28f18c41ed75
Gerrie Swart <g.swart@vehikl.com>
2019-07-16 13:23:23 -0400

    Added initial test

--- typescript/__tests__/Supermarket.test.ts
+++ typescript/__tests__/Supermarket.test.ts
@@ -1,31 +1,29 @@
 import {FakeCatalog} from "./FakeCatalog"
 import {Product} from "../src/model/Product"
 import {SupermarketCatalog} from "../src/model/SupermarketCatalog"
-import {Receipt} from "../src/model/Receipt"
-import {ShoppingCart} from "../src/model/ShoppingCart"
-import {Teller} from "../src/model/Teller"
-import {SpecialOfferType} from "../src/model/SpecialOfferType"
 import {ProductUnit} from "../src/model/ProductUnit"
+import {ShoppingCart} from "../src/model/ShoppingCart";
+import {Teller} from "../src/model/Teller";
+import {SpecialOfferType} from "../src/model/SpecialOfferType";
+import {Receipt} from "../src/model/Receipt";
 
 describe('Supermarket', function () {
-
-    it('TODO decide what to specify', function (this: any) {
-
+    
+    it('applies a three for two discount', function () {
+        const toothbrush: Product = new Product("Toothbrush", ProductUnit.Each);
+        const toothbrushPrice: number = 0.99;
+        const expectedPrice = toothbrushPrice * 2;
         const catalog: SupermarketCatalog = new FakeCatalog();
-        const toothbrush: Product = new Product("toothbrush", ProductUnit.Each);
-        catalog.addProduct(toothbrush, 0.99);
-        const apples: Product = new Product("apples", ProductUnit.Kilo);
-        catalog.addProduct(apples, 1.99);
-
-        const cart: ShoppingCart = new ShoppingCart();
-        cart.addItemQuantity(apples, 2.5);
-
         const teller: Teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+        const cart: ShoppingCart = new ShoppingCart();
 
+        catalog.addProduct(toothbrush, toothbrushPrice);
+        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 10.0);
+        cart.addItemQuantity(toothbrush, 3);
         const receipt: Receipt = teller.checksOutArticlesFrom(cart);
 
-        expect(true).toBe(true);
+        expect(receipt).toMatchSnapshot();
+        expect(receipt.getTotalPrice()).toBeCloseTo(expectedPrice);
     });
 
 });
