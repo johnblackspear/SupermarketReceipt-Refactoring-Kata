07bb40dec9e29c6af15aae171d78e43c1002536b
Gerrie Swart <g.swart@vehikl.com>
2019-07-24 11:49:07 -0400

    Removed direct access to shopping cart internals

--- typescript/src/model/ShoppingCart.ts
+++ typescript/src/model/ShoppingCart.ts
@@ -16,6 +16,10 @@ export class ShoppingCart {
         this.cartContent[product.name] = new ProductAndQuantityTuple(product, quantity);
     }
 
+    public getCountOfProduct(product: Product): number {
+        return this.cartContent[product.name].quantity;
+    }
+
     addDiscountsToReceipt(receipt: Receipt, specialOffers: Offer[], catalog: SupermarketCatalog): void {
 
         for (let offer of specialOffers) {
@@ -34,47 +38,44 @@ export class ShoppingCart {
 
     private twoForAmount(offer: Offer, catalog: SupermarketCatalog, discount: Discount | null) {
 
-        const productAndQuantityTuple = this.cartContent[offer.product.name];
-        const unitPrice: number = catalog.getUnitPrice(productAndQuantityTuple.product);
+        const unitPrice: number = catalog.getUnitPrice(offer.product);
 
-        if (offer.offerType == SpecialOfferType.TwoForAmount && productAndQuantityTuple.quantity >= 2) {
+        if (offer.offerType == SpecialOfferType.TwoForAmount && this.getCountOfProduct(offer.product) >= 2) {
             const x: number = 2;
-            const total = offer.argument * Math.floor(productAndQuantityTuple.quantity / x) + productAndQuantityTuple.quantity % 2 * unitPrice;
-            const discountN = unitPrice * productAndQuantityTuple.quantity - total;
-            discount = new Discount(productAndQuantityTuple.product, "2 for " + offer.argument, discountN);
+            const total = offer.argument * Math.floor(this.getCountOfProduct(offer.product) / x) + this.getCountOfProduct(offer.product) % 2 * unitPrice;
+            const discountN = unitPrice * this.getCountOfProduct(offer.product) - total;
+            discount = new Discount(offer.product, "2 for " + offer.argument, discountN);
         }
         return discount;
     }
 
     private fiveForAmount(offer: Offer, catalog: SupermarketCatalog, discount: Discount | null) {
-        const productAndQuantityTuple = this.cartContent[offer.product.name];
-        const unitPrice: number = catalog.getUnitPrice(productAndQuantityTuple.product);
-        if (offer.offerType == SpecialOfferType.FiveForAmount && productAndQuantityTuple.quantity >= 5) {
+
+        const unitPrice: number = catalog.getUnitPrice(offer.product);
+        if (offer.offerType == SpecialOfferType.FiveForAmount && this.getCountOfProduct(offer.product) >= 5) {
             const x: number = 5;
-            const numberOfXs = Math.floor(productAndQuantityTuple.quantity / x);
-            const discountTotal = unitPrice * productAndQuantityTuple.quantity - (offer.argument * numberOfXs + productAndQuantityTuple.quantity % 5 * unitPrice);
-            discount = new Discount(productAndQuantityTuple.product, x + " for " + offer.argument, discountTotal);
+            const numberOfXs = Math.floor(this.getCountOfProduct(offer.product) / x);
+            const discountTotal = unitPrice * this.getCountOfProduct(offer.product) - (offer.argument * numberOfXs + this.getCountOfProduct(offer.product) % 5 * unitPrice);
+            discount = new Discount(offer.product, x + " for " + offer.argument, discountTotal);
         }
         return discount;
     }
 
     private percentageDiscount(offer: Offer, catalog: SupermarketCatalog, discount: Discount | null) {
-        const productAndQuantityTuple = this.cartContent[offer.product.name];
-        const unitPrice: number = catalog.getUnitPrice(productAndQuantityTuple.product);
+        const unitPrice: number = catalog.getUnitPrice(offer.product);
         if (offer.offerType == SpecialOfferType.PercentageDiscount) {
-            discount = new Discount(productAndQuantityTuple.product, offer.argument + "% off", productAndQuantityTuple.quantity * unitPrice * offer.argument / 100.0);
+            discount = new Discount(offer.product, offer.argument + "% off", this.getCountOfProduct(offer.product) * unitPrice * offer.argument / 100.0);
         }
         return discount;
     }
 
     private threeForTwo(offer: Offer, catalog: SupermarketCatalog, discount: Discount | null) {
-        const productAndQuantityTuple = this.cartContent[offer.product.name];
-        const unitPrice: number = catalog.getUnitPrice(productAndQuantityTuple.product);
-        if (offer.offerType == SpecialOfferType.ThreeForTwo && productAndQuantityTuple.quantity > 2) {
+        const unitPrice: number = catalog.getUnitPrice(offer.product);
+        if (offer.offerType == SpecialOfferType.ThreeForTwo && this.getCountOfProduct(offer.product) > 2) {
             const x: number = 3;
-            const numberOfXs = Math.floor(productAndQuantityTuple.quantity / x);
-            const discountAmount = productAndQuantityTuple.quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + productAndQuantityTuple.quantity % 3 * unitPrice);
-            discount = new Discount(productAndQuantityTuple.product, "3 for 2", discountAmount);
+            const numberOfXs = Math.floor(this.getCountOfProduct(offer.product) / x);
+            const discountAmount = this.getCountOfProduct(offer.product) * unitPrice - ((numberOfXs * 2 * unitPrice) + this.getCountOfProduct(offer.product) % 3 * unitPrice);
+            discount = new Discount(offer.product, "3 for 2", discountAmount);
         }
         return discount;
     }
